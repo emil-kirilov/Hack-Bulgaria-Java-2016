76db709d9c878b4953f3318feb301b6b19c9a3fa
Emil Kirilov <emil.kirilovk@gmail.com>
2016-11-06 15:34:47 +0200

    Linked List

---
+++ hackbulgaria/src/week3/LinkedListInterface.java
@@ -0,0 +1,13 @@
+package week3;
+
+public interface LinkedListInterface<T> {
+	public void addFirst(T newElement);
+	public void addLast(T newElement);
+	public void add(T newElement, int index);
+	public T getFirst();
+	public T getLast();
+	public T get(int index);
+	public int size();
+	public void remove(int index);
+	public void addList(LinkedListInterface<T> list);
+}
